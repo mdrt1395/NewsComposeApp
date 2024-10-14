7badf4f1873e1284ec3a02c3e05394a2bf8f5e9e
mdrt1395 <marcodavidrt95@gmail.com>
2024-10-13 19:42:06 -0600

    dependencies to be changed

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -18,30 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NewsAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsAppTheme {
-        Greeting("Android")
-    }
-}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,18 @@
+object Dependencies {
+
+    val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
+    val appcompat by lazy { "androidx.appcompat:appcompat:${Versions.appcompat}" }
+    val material by lazy { "com.google.android.material:material:${Versions.material}" }
+    val lifecycleRuntimeKtx by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntimeKtx}" }
+    val activityCompose by lazy { "androidx.activity:activity-compose:${Versions.activityCompose}" }
+    val composeBom by lazy { "androidx.compose:compose-bom:${Versions.compose}" }
+    val composeUi by lazy { "androidx.compose.ui:ui:1.7.3" }
+    val composeUiGraphics by lazy {"androidx.compose.ui:ui-graphics:1.7.3"}
+    val composeUiToolingPreview by lazy { "androidx.compose.ui:ui-tooling-preview:1.7.3" }
+    val composeMaterial3 by lazy { "androidx.compose.material3:material3:1.3.0" }
+    val navigationCompose by lazy { "androidx.navigation:navigation-compose:${Versions.navigationCompose}" }
+    val composeTestJUnit4 by lazy { "androidx.compose.ui:ui-test:${Versions.compose}" }
+    val composeUiTooling by lazy { "androidx.compose.ui:ui-tooling:${Versions.compose}" }
+    val composeUiTestManifest by lazy {"androidx.compose.ui:ui-test-manifest:${Versions.compose}"}
+    val lifecycleViewModelKtx by lazy { "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleViewModelKtx}" }
+}
