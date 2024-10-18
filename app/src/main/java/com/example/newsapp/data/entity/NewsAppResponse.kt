e17097d88bf302ce02d5447017e1c1725a02d471
mdrt1395 <marcodavidrt95@gmail.com>
2024-10-18 12:59:58 -0600

    null safe checked NewsAppResponse

--- app/src/main/java/com/example/newsapp/data/entity/NewsAppResponse.kt
+++ app/src/main/java/com/example/newsapp/data/entity/NewsAppResponse.kt
@@ -7,17 +7,17 @@ data class NewsAppResponse(
 )
 
 data class Article (
-    val author: String,
-    val title: String,
+    val author: String?= null,
+    val title: String?= null,
     val description: String? = null,
-    val url: String,
+    val url: String?= null,
     val urlToImage:String?= null,
-    val publishedAt: String,
+    val publishedAt: String?= null,
     val content: String?= null,
     val source: Source
 )
 
 data class Source (
-        val id: String,
-    val name: String
+        val id: String?= null,
+    val name: String?= null
         )
